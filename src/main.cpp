bfc5aa5150c6e89f27e212b820b0745f2c53ed6e
megamilkx <gyrocopter3k@gmail.com>
2017-02-09 10:54:10 +0300

    Update test

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,60 @@
 class IndexPage : public HTTPModule
 {
 public:
-    std::string HTML()
+    void Params(const std::map<std::string, std::string>& params)
     {
-        return R"(
-            <!DOCTYPE html>
-            <html>
-            <head>
-            <meta charset="UTF-8">
-            <title>Title of the document</title>
-            </head>
-
-            <body>
-            Content of the document......
-            </body>
-
-            </html> 
-        )";
+        this->params = params;
     }
+
+    std::string Title()
+    {
+        return "D&D Thing";
+    }
+    
+    std::string Head()
+    {
+        std::string head;
+        
+        head += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">";
+        
+        return head;
+    }
+    
+    std::string Body()
+    {
+        std::string body;
+        body += "<h1>D&D Tool</h1>";
+        
+        body += "<table border=\"1\">";
+        body += "<tr class=\"table-title\">";
+        
+        std::map<std::string, std::string>::const_iterator it = params.begin();
+        for(it; it != params.end(); ++it)
+        {
+            body += "<td>";
+            body += it->first;
+            body += "</td>";
+        }
+        
+        body += "</tr>";
+        body += "<tr>";
+        
+        it = params.begin();
+        for(it; it != params.end(); ++it)
+        {
+            body += "<td>";
+            body += it->second;
+            body += "</td>";
+        }
+        
+        body += "</tr>";
+        body += "</table>";
+        
+        return body;
+    }
+    
+private:
+    std::map<std::string, std::string> params;
 };
 
 int main()
@@ -27,6 +64,11 @@ int main()
     HTTPServer server;
     server.Init();
     server.AddModule<IndexPage>("/");
+    //server.AddModule<FileGet>("GET");
+    
+    server.AddMIME("css", "text/css");
+    server.AddMIME("ico", "image/x-icon");
+    server.AddMIME("png", "image/png");
     
     server.Run();
     
