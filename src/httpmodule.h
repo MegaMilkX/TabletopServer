57073fe1e574692203ef1a7afc9a7af78e27e4b0
megamilkx <gyrocopter3k@gmail.com>
2017-02-09 10:52:42 +0300

    Add Params(), Title() and Body() to HTTPModule

--- src/httpmodule.h
+++ src/httpmodule.h
@@ -1,11 +1,30 @@
 #ifndef HTTPMODULE_H
 #define HTTPMODULE_H
 
+#include <map>
+
 class HTTPModule
 {
 public:
     virtual ~HTTPModule() {}
-    virtual std::string HTML() = 0;
+    virtual void Params(const std::map<std::string, std::string>& params) {}
+    virtual std::string HTML()
+    {
+        std::string html;
+        html += "<!DOCTYPE html>";
+        html += "<html>";
+        html += "<head>";
+        html += "<meta charset=\"UTF-8\">";
+        html += "<title>" + Title() + "</title>";
+        html += Head();
+        html += "</head>";
+        html += "<body>" + Body() + "</body>";
+        html += "</html>";
+        return html;
+    }
+    virtual std::string Title() { return "Title"; }
+    virtual std::string Head() { return ""; }
+    virtual std::string Body() { return "Document body"; }
 };
 
 #endif
