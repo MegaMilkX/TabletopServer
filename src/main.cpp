45668d1d6445e918b5ec537a1b63533003539100
megamilkx <gyrocopter3k@gmail.com>
2017-02-10 00:01:57 +0300

    Update test

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,28 @@
 #include "httpserver.h"
 
-class IndexPage : public HTTPModule
+class IndexHandler : public HTTPRequestHandler
 {
 public:
-    void Params(const std::map<std::string, std::string>& params)
+    void operator()(const HTTPRequest& request, HTTPResponse& response)
     {
-        this->params = params;
+        response.Header("Content-Type", "text/html; charset=UTF-8");
+        response.Data("hello world");
     }
-
-    std::string Title()
-    {
-        return "D&D Thing";
-    }
-    
-    std::string Head()
-    {
-        std::string head;
-        
-        head += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">";
-        
-        return head;
-    }
-    
-    std::string Body()
-    {
-        std::string body;
-        body += "<h1>D&D Tool</h1>";
-        
-        body += "<table border=\"1\">";
-        body += "<tr class=\"table-title\">";
-        
-        std::map<std::string, std::string>::const_iterator it = params.begin();
-        for(it; it != params.end(); ++it)
-        {
-            body += "<td>";
-            body += it->first;
-            body += "</td>";
-        }
-        
-        body += "</tr>";
-        body += "<tr>";
-        
-        it = params.begin();
-        for(it; it != params.end(); ++it)
-        {
-            body += "<td>";
-            body += it->second;
-            body += "</td>";
-        }
-        
-        body += "</tr>";
-        body += "</table>";
-        
-        return body;
-    }
-    
-private:
-    std::map<std::string, std::string> params;
 };
 
 int main()
 {
     HTTPServer server;
     server.Init();
-    server.AddModule<IndexPage>("/");
-    //server.AddModule<FileGet>("GET");
+    
+    server.AddHandler<IndexHandler>("/");
     
     server.AddMIME("css", "text/css");
     server.AddMIME("ico", "image/x-icon");
     server.AddMIME("png", "image/png");
+    server.AddMIME("html", "text/html");
+    server.AddMIME("js", "application/javascript");
+    server.AddMIME("jpg", "image/jpg");
     
     server.Run();
     
