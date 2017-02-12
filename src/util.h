89d23329c18c299e5ddcd1e9e1872d0397c85360
megamilkx <gyrocopter3k@gmail.com>
2017-02-12 21:20:43 +0300

    Add util trim

--- src/util.h
+++ src/util.h
@@ -3,6 +3,10 @@
 
 #include <string>
 #include <vector>
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
 
 namespace util
 {
@@ -67,6 +71,28 @@ inline std::string urlDecode(std::string &SRC)
         }
     }
     return (ret);
+}
+
+// trim from start
+inline std::string &ltrim(std::string &s) 
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+// trim from end
+inline std::string &rtrim(std::string &s) 
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
+}
+
+// trim from both ends
+inline std::string &trim(std::string &s) 
+{
+    return ltrim(rtrim(s));
 }
 
 }
